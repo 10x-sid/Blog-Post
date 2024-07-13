93b8a098954526d929d4b9d7d7ebc3a6874ac11e
sudhanshu20204 <sidshekhar2024@gmail.com>
2024-07-13 11:16:43 +0530

    update the sigup page

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,20 +1,34 @@
 import  { ChangeEvent } from 'react'
+
+
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 interface input{
     title:string,
     placeholder:string,
     onchange: (e:ChangeEvent<HTMLInputElement>) => void,
-    type?:string
+    type?:string,
+    buttonClas?:boolean;
+    button?: boolean;
+    onclick?:()=>void
 
 }
-export default function Input({title,placeholder,onchange,type}:input){
+//to put the button and input in same use relative inline block to wrapper and abousulte and right-0 to buttton
+export default function Input({title,placeholder,onchange,type,button,buttonClas,onclick}:input){
     return(
-        <div className='mt-3 w-auto'>
+        <div className='mt-3 w-auto'>  
             <div className="text-black text-sm font-bold">
                 {title}
             </div>
-            <input 
-             onChange={onchange} type={type ||"text"} placeholder={placeholder} className=" border-2  rounded w-auto py-2 pr-24 pl-3 mt-1 text-gray-700 leading-tight "></input>
-             
+           <div className=' relative inline-block '>
+                <input 
+                    onChange={onchange} type={type ||"text"} placeholder={placeholder} className=" border-2  rounded w-auto py-2 pr-24 pl-3 mt-1 text-gray-700 leading-tight ">
+
+                </input>
+                {button? <button className='absolute h-full items-center top-0 right-0 px-2 text-gray-500 ' onClick={onclick}>
+                        {buttonClas? <FaEyeSlash /> : <FaEye />}
+                </button>:<></>}
+                
+           </div>
         </div>
     )
 
--- Frontend/src/components/UpInput.tsx
+++ Frontend/src/components/UpInput.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import { signupTypes } from "@shekharsid/blog-post"
 
 
+
 export const Upinput=()=>{
     const [passwordVisible,setVisible]=useState(false)
     const [inputs,setInputs]=useState<signupTypes>({ //pass types of signup inputs as genric to the state varible so  that we change while mainting the types of the varibles
@@ -17,7 +18,7 @@ export const Upinput=()=>{
         setVisible(!passwordVisible)
     }
     return(
-        <div className="w-auto">
+        <div className="w-full">
             <div className="h-screen flex flex-col items-center justify-center w-auto ">
                 <div className=" flex">
                     <div className="text-black font-bold text-3xl mt-4 mx-2"> Create an account </div>
@@ -28,42 +29,34 @@ export const Upinput=()=>{
                 <div className="text-slate-400 pt-1 font-semibold ">
                     Already have an account ? <Link className="underline" to="/signin">Login</Link>
                 </div>
-                <div className="px-8">
-                    <Input title="Name" placeholder="Alex" onchange={(e)=>{
-                        setInputs((c)=>(
-                            {...c,
-                            name:e.target.value}//we can't use diff use state cuse of type error
-                        ))
+                <div>
+                        <Input title="Name" placeholder="Alex" onchange={(e)=>{
+                            setInputs((c)=>(
+                                {...c,
+                                name:e.target.value}//we can't use diff use state cuse of type error
+                            ))
 
-                    }}/>
-                    <Input title="Email" placeholder="Alex@gmail.com" onchange={(e)=>{
-                        setInputs((c)=>( // we use c=>() and not c=>{} cause we want to return the new changes value to setInputs!!
-                            {...c,
-                            email:e.target.value}
-                        ))
+                        }}/>
+                        <Input title="Email" placeholder="Alex@gmail.com" onchange={(e)=>{
+                            setInputs((c)=>( // we use c=>() and not c=>{} cause we want to return the new changes value to setInputs!!
+                                {...c,
+                                email:e.target.value}
+                            ))
 
                     }}/>
                     <div className="flex">
-                        <Input title="password" placeholder="min-6" type={passwordVisible? "text":"password"} onchange={(e)=>{
+                        <Input title="password" placeholder="min-6" type={ passwordVisible? "text": "password"} button={true} onclick={()=>togglePasswordVisibility()} buttonClas={passwordVisible} onchange={(e)=>{
                             setInputs((c)=>(
                                 {...c,//spread operator will maintain the current value of other things
                                 password:e.target.value}
                             ))
 
                         }}/>
-
-                        {/* <button
-                                type="button"
-                                onClick={togglePasswordVisibility}
-                                className=" ml-2 px-3 py-2 inset-y-0 right-0  mt-6 flex items-center focus:outline-none">
-                                {passwordVisible ? <FaEyeSlash /> : <FaEye />}
-                        </button> */}
-
-                        
-            
                     </div>
-                        <button className="rounded bg-black text-white text-bold items-center mt-7 py-2 mr-10 w-full">Sign Up</button>
-            
+                        <button className="rounded bg-black text-white text-bold items-center mt-7 py-2 mr-10 w-full">
+                            Sign Up
+                        </button>
+        
 
                 
             </div>
